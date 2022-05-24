05fc1b21552092b04cc3eaa207ec57c22f0cfe35
Lauren Ponisio <lponisio@gmail.com>
2022-05-24 15:40:41 -0700

    moved packages to init file to clear up scripts

---
+++ analyses/src/init.R
@@ -0,0 +1,16 @@
+## models
+library(brms)
+library(bayesplot)
+library(tidybayes)
+library(mice)
+library(ggplot2)
+
+
+## plotting
+library(tidyr)
+library(dplyr)
+library(viridis)
+library(tidybayes)
+library(gridExtra)
+library(grid)
+library(scales)
