db0381cd04087adbbe830b03205d92a3f58a5d71
Lauren Ponisio <lponisio@gmail.com>
2022-05-24 15:39:52 -0700

    re worked libraries files to be via bah

--- dataPrep/libraries.R
+++
@@ -1,8 +0,0 @@
-rm(list=ls())
-## install and load libraries for all analyses
-install.packages("vegan")
-install.packages("piecewiseSEM")
-install.packages("lme4")
-install.packages("lmerTest")
-install.packages("brms")
-
