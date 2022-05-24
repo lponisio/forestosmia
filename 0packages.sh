9d70a14434a889ea0868bc0a6e75a44fb20b07b7
Lauren Ponisio <lponisio@gmail.com>
2022-05-24 15:40:01 -0700

    re worked libraries files to be via bah

---
+++ 0packages.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+Rscript -e 'install.packages("dplyr", repos="http://cran.r-project.org")'
+Rscript -e 'install.packages("viridis", repos="http://cran.r-project.org")'
+Rscript -e 'install.packages("ggplot2", repos="http://cran.r-project.org")'
+Rscript -e 'install.packages("brms", repos="http://cran.r-project.org")'
+Rscript -e 'install.packages("bayesplot", repos="http://cran.r-project.org")'
+Rscript -e 'install.packages("tidybayes", repos="http://cran.r-project.org")'
+Rscript -e 'install.packages("mice", repos="http://cran.r-project.org")'
+Rscript -e 'install.packages("grid", repos="http://cran.r-project.org")'
+Rscript -e 'install.packages("gridExtra", repos="http://cran.r-project.org")'
+Rscript -e 'install.packages("scales", repos="http://cran.r-project.org")'
+Rscript -e 'install.packages("tidyr", repos="http://cran.r-project.org")'
