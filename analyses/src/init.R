454b828c4a8c98caf3f696d690d82d46739c0bc0
Lauren Ponisio <lponisio@gmail.com>
2023-08-23 10:30:03 -0700

    created functions for running each parasite SEM, and a combined model

--- analyses/src/init.R
+++ analyses/src/init.R
@@ -1,8 +1,11 @@
 ## models
 library(brms)
+library(mice)
 library(bayesplot)
 library(tidybayes)
 library(bayestestR)
+library(tidyr)
+library(dplyr)
 
 ## plotting
 library(ggplot2)
@@ -11,6 +14,7 @@ library(grid)
 library(ggthemes)
 library(scales)
 library(RColorBrewer)
+library(gridExtra)
 
 dir.create("figures/diagnostics", showWarnings = FALSE)
 dir.create("saved/tables", showWarnings = FALSE)
