d76cf5d76675cc94826ffaff6cf200c8667e8501
Lauren Ponisio <lponisio@gmail.com>
2023-09-01 14:24:37 -0700

    cleaned up repo in preparation for publication

--- 0packages.sh
+++ 0packages.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
 Rscript -e 'install.packages("dplyr", repos="http://cran.r-project.org")'
-Rscript -e 'install.packages("viridis", repos="http://cran.r-project.org")'
-Rscript -e 'install.packages("ggplot2", repos="http://cran.r-project.org")'
 Rscript -e 'install.packages("brms", repos="http://cran.r-project.org")'
 Rscript -e 'install.packages("bayesplot", repos="http://cran.r-project.org")'
 Rscript -e 'install.packages("tidybayes", repos="http://cran.r-project.org")'
@@ -11,3 +9,9 @@ Rscript -e 'install.packages("grid", repos="http://cran.r-project.org")'
 Rscript -e 'install.packages("gridExtra", repos="http://cran.r-project.org")'
 Rscript -e 'install.packages("scales", repos="http://cran.r-project.org")'
 Rscript -e 'install.packages("tidyr", repos="http://cran.r-project.org")'
+
+## plotting
+Rscript -e 'install.packages("ggplot2", repos="http://cran.r-project.org")'
+Rscript -e 'install.packages("lemon", repos="http://cran.r-project.org")'
+Rscript -e 'install.packages("egg", repos="http://cran.r-project.org")'
+Rscript -e 'install.packages("RColorBrewer", repos="http://cran.r-project.org")'
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # forestosmia
-Collaborative project with Dr. Jim Rivers Lab at OSU. Examining how forest and timber mgmt impacts health of Osmia bees. 
+
+Analysis code for understanding the potential for harvested forests to
+contribute to supporting health bee populations from the publication:
+"Beyond timber: forest management can support healthy bee populations"
+(doi:)
+
+The data accompanies the code for reproducibility, but we ask to be
+contacted (lponisio@gmail.com and Jim.Rivers@oregonstate.edu) if
+authors wish to use the data for a publication.
+
+The R scripts in analysis should be run in order (i.e., 0, 1, 2) to
+reproduce the analyses and figures.  All of the packages needed to run
+the analyses are listed in the packages.sh file in the main
+directory. All analyses were run using R (version 4.2.2).
